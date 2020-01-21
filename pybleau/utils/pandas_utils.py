92a6121dce8b33692d71e27c0be0a46d8f5874d4
Jonathan Rocher <jrocher@kbibiopharma.com>
2020-01-21 11:55:00 -0600

    Add app entry point.

--- pybleau/utils/pandas_utils.py
+++ pybleau/utils/pandas_utils.py
@@ -1,4 +1,9 @@
+import logging
 from six import string_types
+import pandas as pd
+from os.path import isfile, splitext
+
+logger = logging.getLogger(__name__)
 
 
 def is_string_series(series, data=None):
@@ -20,3 +25,37 @@ def get_col(df, col_name):
         return df.index
     else:
         return df[col_name]
+
+
+def pd_read_any(target, pandas_func=None, **kwargs):
+    """ Wraps pd.read_csv, pd.read_excel, ... Load DF from any supported format
+
+    Parameters
+    ----------
+    target : str
+        Path to the file to load.
+
+    kwargs : dict
+        Keywords passed to the pandas read_** function. Refer to pandas
+        documentation.
+    """
+    if not isfile(target):
+        msg = "File not found: {}".format(target)
+        logger.exception(msg)
+        raise IOError(msg)
+
+    if pandas_func is None:
+        ext = splitext(target)[1].lower()
+        if ext in [".xls", ".xlsx"]:
+            pandas_func = pd.read_excel
+        elif ext == ".csv":
+            pandas_func = pd.read_csv
+        elif ext in [".h5", ".hdf"]:
+            pandas_func = pd.read_hdf
+        else:
+            msg = "Extension {} not supported by pd_read_any.".format(ext)
+            logger.error(msg)
+            return
+
+    target = pandas_func(target, **kwargs)
+    return target
