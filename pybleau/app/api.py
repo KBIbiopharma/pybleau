e1dba627e31b6ee7c9dd3da74c6ed21aa202768e
Jonathan Rocher <jrocher@kbibiopharma.com>
2020-06-22 16:41:35 -0500

    Add regression test. (Only fails on py2)

--- pybleau/app/api.py
+++ pybleau/app/api.py
@@ -2,4 +2,5 @@ from .ui.dataframe_analyzer_model_view import DataFrameAnalyzer, DataFrameAnalyz
 from .model.dataframe_plot_manager import DataFramePlotManager  # noqa
 from .ui.dataframe_plot_manager_view import DataFramePlotManagerView  # noqa
 from .tools.filter_expression_manager import FilterExpression, FilterExpressionManager  # noqa
+from .model.plot_descriptor import PlotDescriptor  # noqa
 from .app.main import main  # noqa
