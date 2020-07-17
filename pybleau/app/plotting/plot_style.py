69b105da2bf6ae8e381132b9de0cc5f0618ff32d
Jonathan Rocher <jrocher@kbibiopharma.com>
2020-07-17 16:42:36 -0500

    Fix BaseColorXYPlotStyle use of Enum.

--- pybleau/app/plotting/plot_style.py
+++ pybleau/app/plotting/plot_style.py
@@ -314,7 +314,8 @@ class BaseColorXYPlotStyle(BaseXYPlotStyle):
     """
     #: Name of the palette to pick colors when generating multiple renderers
     # (Ignored when colorizing by a float: then the renderer's palette is used)
-    color_palette = Enum(DEFAULT_DIVERG_PALETTE, values=ALL_MPL_PALETTES)
+    color_palette = Enum(*ALL_MPL_PALETTES,
+                         default_value=DEFAULT_DIVERG_PALETTE)
 
     #: Font used to draw the color dimension title
     color_axis_title_style = Instance(TitleStyle, ())
