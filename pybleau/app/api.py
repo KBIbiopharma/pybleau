206a983323192d3a2c55ed3aa49b021905049953
Jonathan Rocher <jrocher@kbibiopharma.com>
2020-07-21 17:33:35 -0500

    Add multi-df analyzer to the pybleau API.

--- pybleau/app/api.py
+++ pybleau/app/api.py
@@ -1,5 +1,7 @@
 from .ui.dataframe_analyzer_model_view import DataFrameAnalyzer, DataFrameAnalyzerView  # noqa
 from .model.dataframe_plot_manager import DataFramePlotManager  # noqa
+from .model.multi_dfs_dataframe_analyzer import MultiDataFrameAnalyzer
 from .ui.dataframe_plot_manager_view import DataFramePlotManagerView  # noqa
 from .tools.filter_expression_manager import FilterExpression, FilterExpressionManager  # noqa
 from .app.main import main  # noqa
+
