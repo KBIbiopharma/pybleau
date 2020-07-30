7ae7a6d79ec2b99524617d04e4e27c5dd149f323
Jonathan Rocher <jrocher@kbibiopharma.com>
2020-07-30 17:26:37 -0500

    Add more tests.

--- pybleau/app/model/tests/test_dataframe_analyzer.py
+++ pybleau/app/model/tests/test_dataframe_analyzer.py
@@ -1,59 +1,56 @@
 from unittest import skipIf, TestCase
 import os
 
-from .test_base_dataframe_analyzer import Analyzer, \
-    DisplayingDataFrameAnalyzer, FilterDataFrameAnalyzer, \
+from .base_dataframe_analyzer import Analyzer, DisplayingDataFrameAnalyzer, \
+    FilterDataFrameAnalyzer, SelectionPlotDataFrameAnalyzer, \
     SortingDataFrameAnalyzer, SummaryDataFrameAnalyzer
-
-try:
-    import kiwisolver  # noqa
-    KIWI_AVAILABLE = True
-except ImportError:
-    KIWI_AVAILABLE = False
 
 BACKEND_AVAILABLE = os.environ.get("ETS_TOOLKIT", "qt4") != "null"
 
-if BACKEND_AVAILABLE and KIWI_AVAILABLE:
+if BACKEND_AVAILABLE:
     from pybleau.app.model.dataframe_analyzer import DataFrameAnalyzer
 
-msg = "No UI backend to paint into or no Kiwisolver"
+msg = "No UI backend to paint into"
 
 
-@skipIf(not BACKEND_AVAILABLE or not KIWI_AVAILABLE, msg)
+@skipIf(not BACKEND_AVAILABLE, msg)
 class TestAnalyzer(Analyzer, TestCase):
-
     @classmethod
     def setUpClass(cls):
         cls.analyzer_klass = DataFrameAnalyzer
 
 
-@skipIf(not BACKEND_AVAILABLE or not KIWI_AVAILABLE, msg)
+@skipIf(not BACKEND_AVAILABLE, msg)
 class TestFilterDataFrameAnalyzer(FilterDataFrameAnalyzer, TestCase):
-
     @classmethod
     def setUpClass(cls):
         cls.analyzer_klass = DataFrameAnalyzer
 
 
-@skipIf(not BACKEND_AVAILABLE or not KIWI_AVAILABLE, msg)
+@skipIf(not BACKEND_AVAILABLE, msg)
 class TestSummaryDataFrameAnalyzer(SummaryDataFrameAnalyzer, TestCase):
-
     @classmethod
     def setUpClass(cls):
         cls.analyzer_klass = DataFrameAnalyzer
 
 
-@skipIf(not BACKEND_AVAILABLE or not KIWI_AVAILABLE, msg)
+@skipIf(not BACKEND_AVAILABLE, msg)
 class TestSortingDataFrameAnalyzer(SortingDataFrameAnalyzer, TestCase):
-
     @classmethod
     def setUpClass(cls):
         cls.analyzer_klass = DataFrameAnalyzer
 
 
-@skipIf(not BACKEND_AVAILABLE or not KIWI_AVAILABLE, msg)
+@skipIf(not BACKEND_AVAILABLE, msg)
 class TestDisplayingDataFrameAnalyzer(DisplayingDataFrameAnalyzer, TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.analyzer_klass = DataFrameAnalyzer
 
+
+@skipIf(not BACKEND_AVAILABLE, msg)
+class TestSelectionPlotDataFrameAnalyzer(SelectionPlotDataFrameAnalyzer,
+                                         TestCase):
     @classmethod
     def setUpClass(cls):
         cls.analyzer_klass = DataFrameAnalyzer
