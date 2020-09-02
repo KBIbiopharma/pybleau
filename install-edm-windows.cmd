e6a1146b37ca84033ab62f5ce90333065fc168af
Jonathan Rocher <jrocher@kbibiopharma.com>
2020-09-02 16:54:44 -0500

    Test executing another powershell command.

--- install-edm-windows.cmd
+++ install-edm-windows.cmd
@@ -6,6 +6,9 @@ SET INSTALL_EDM_VERSION=3.0.1
 SET EDM_MAJOR_MINOR=3.0
 SET EDM_PACKAGE=edm_cli_%INSTALL_EDM_VERSION%_x86_64.msi
 SET EDM_INSTALLER_PATH=%HOMEDRIVE%%HOMEPATH%\.cache\%EDM_PACKAGE%
+
+CALL powershell.exe -Command "dir %HOMEDRIVE%%HOMEPATH%\.cache"
+
 SET URL=https://package-data.enthought.com/edm/win_x86_64/%EDM_MAJOR_MINOR%/%EDM_PACKAGE%
 SET COMMAND="(new-object net.webclient).DownloadFile('%URL%', '%EDM_INSTALLER_PATH%')"
 
