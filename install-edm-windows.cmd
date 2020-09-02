e62749962e9160f6a05c924c6c67dcf2b34709a7
Jonathan Rocher <jrocher@kbibiopharma.com>
2020-09-02 14:59:54 -0500

    Refactor script to be more readable.

--- install-edm-windows.cmd
+++ install-edm-windows.cmd
@@ -6,7 +6,8 @@ SET INSTALL_EDM_VERSION=3.0.1
 SET EDM_MAJOR_MINOR=3.0
 SET EDM_PACKAGE=edm_cli_%INSTALL_EDM_VERSION%_x86_64.msi
 SET EDM_INSTALLER_PATH=%HOMEDRIVE%%HOMEPATH%\.cache\%EDM_PACKAGE%
-SET COMMAND="(new-object net.webclient).DownloadFile('https://package-data.enthought.com/edm/win_x86_64/%EDM_MAJOR_MINOR%/%EDM_PACKAGE%', '%EDM_INSTALLER_PATH%')"
+SET URL = https://package-data.enthought.com/edm/win_x86_64/%EDM_MAJOR_MINOR%/%EDM_PACKAGE%
+SET COMMAND="(new-object net.webclient).DownloadFile('%URL%', '%EDM_INSTALLER_PATH%')"
 
 IF NOT EXIST %EDM_INSTALLER_PATH% CALL powershell.exe -Command %COMMAND% || GOTO error
 CALL msiexec /qn /i %EDM_INSTALLER_PATH% EDMAPPDIR=C:\Enthought\edm || GOTO error
