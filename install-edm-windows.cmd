108d1999d6c352045739e50466a24630276f6e39
Jonathan Rocher <jrocher@kbibiopharma.com>
2020-09-02 15:35:01 -0500

    Try again install command.

--- install-edm-windows.cmd
+++ install-edm-windows.cmd
@@ -10,9 +10,7 @@ SET URL=https://package-data.enthought.com/edm/win_x86_64/%EDM_MAJOR_MINOR%/%EDM
 SET COMMAND="(new-object net.webclient).DownloadFile('%URL%', '%EDM_INSTALLER_PATH%')"
 
 IF NOT EXIST %EDM_INSTALLER_PATH% CALL powershell.exe -Command %COMMAND% || GOTO error
-
-SET EDMAPPDIR=edm
-CALL msiexec /qn /i %EDM_INSTALLER_PATH% EDMAPPDIR=%EDMAPPDIR% || GOTO error
+CALL msiexec /qn /i %EDM_INSTALLER_PATH% EDMAPPDIR=edm || GOTO error
 
 ENDLOCAL
 @ECHO.DONE
