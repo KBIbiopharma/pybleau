85425bd4090ae82d5ef90a28eb2e12b7892580ef
Jonathan Rocher <jrocher@kbibiopharma.com>
2020-09-02 17:07:11 -0500

    Don't try to download into non-existent .cache folder.

--- install-edm-windows.cmd
+++ install-edm-windows.cmd
@@ -5,9 +5,7 @@ SETLOCAL EnableDelayedExpansion
 SET INSTALL_EDM_VERSION=3.0.1
 SET EDM_MAJOR_MINOR=3.0
 SET EDM_PACKAGE=edm_cli_%INSTALL_EDM_VERSION%_x86_64.msi
-SET EDM_INSTALLER_PATH=%HOMEDRIVE%%HOMEPATH%\.cache\%EDM_PACKAGE%
-
-CALL powershell.exe -Command "dir %HOMEDRIVE%%HOMEPATH%\.cache"
+SET EDM_INSTALLER_PATH=%HOMEDRIVE%%HOMEPATH%\%EDM_PACKAGE%
 
 SET URL=https://package-data.enthought.com/edm/win_x86_64/%EDM_MAJOR_MINOR%/%EDM_PACKAGE%
 SET COMMAND="(new-object net.webclient).DownloadFile('%URL%', '%EDM_INSTALLER_PATH%')"
