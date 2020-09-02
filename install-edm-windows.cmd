01cf5793da05b3f27e402401b11a27c849049fda
Jonathan Rocher <jrocher@kbibiopharma.com>
2020-09-02 14:49:32 -0500

    Fix missing env var.

--- install-edm-windows.cmd
+++ install-edm-windows.cmd
@@ -3,8 +3,7 @@
 SETLOCAL EnableDelayedExpansion
 
 SET INSTALL_EDM_VERSION=3.0.1
-
-SET EDM_MAJOR_MINOR=%MAJOR%.%MINOR%
+SET EDM_MAJOR_MINOR=3.0
 SET EDM_PACKAGE=edm_cli_%INSTALL_EDM_VERSION%_x86_64.msi
 SET EDM_INSTALLER_PATH=%HOMEDRIVE%%HOMEPATH%\.cache\%EDM_PACKAGE%
 SET COMMAND="(new-object net.webclient).DownloadFile('https://package-data.enthought.com/edm/win_x86_64/%EDM_MAJOR_MINOR%/%EDM_PACKAGE%', '%EDM_INSTALLER_PATH%')"
