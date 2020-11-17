ddc7375533daab67f1f195350bae8f59bc5cdf0c
Jason Chambless <jchambless@kbibiopharma.com>
2020-11-17 16:52:50 -0600

    Update filter_expression_editor.py
    Working, but needs to be cleaned up.

--- pybleau/app/ui/filter_expression_editor.py
+++ pybleau/app/ui/filter_expression_editor.py
@@ -1,6 +1,5 @@
 import logging
 from collections import Iterable
-from operator import itemgetter
 
 from app_common.std_lib.str_utils import sanitize_string
 from pandas import DataFrame
@@ -9,13 +8,54 @@ from pyface.api import Clipboard
 from traits.api import Enum
 from traits.api import HasStrictTraits, Instance, List, Button, Bool, Str, \
     Any, Dict
+from traits.trait_types import Callable
 from traitsui.api import VGroup, Item, OKCancelButtons, View, Tabbed, \
     HSplit, StatusItem, HGroup, Spring
+from traitsui.editors import InstanceEditor
 
 COPY_TO_CLIPBOARD = "copy to clipboard"
 APPEND_TO_FILTER = "append to filter"
 
 logger = logging.getLogger(__name__)
+
+
+class ScrollableButtonListView(HasStrictTraits):
+    """ Provides a view for a dict of str:str conversions to Buttons
+
+    Give this class a `dict`, and it will turn those `key:value` pairs
+    into a view full of `Buttons`. Clicking any `Button` calls the
+    `handler` with the key as the `event.name`.
+    The keys must be strings that are usable as trait names; the values can
+    be any string.
+    """
+
+    #: Dict of trait-valid name strings as keys and any string as values
+    traits_and_names = Dict
+
+    #: Function to call when any of the resulting `Button` objects are clicked
+    handler = Callable
+
+    #: Class to use to create TraitsUI window to open controls
+    view_klass = Any(default_value=View)
+
+    #: Name for the VGroup
+    group_label = Str
+
+    def traits_view(self):
+        trait_names = []
+
+        # add the trait names to this object, and connect the handler function
+        for trait_name, label in self.traits_and_names.items():
+            trait_names.append(trait_name)
+            self.add_trait(trait_name, Button(label))
+            self.observe(self.handler, trait_name)
+        # create an Item for each added Button trait
+        items = [Item(n, show_label=False) for n in trait_names]
+
+        return self.view_klass(
+            VGroup(*items, label=self.group_label),
+            scrollable=True
+        )
 
 
 class FilterExpressionEditorView(HasStrictTraits):
@@ -61,6 +101,9 @@ class FilterExpressionEditorView(HasStrictTraits):
     #: List of actions that can occur when clicking a button
     _click_action_options = List([APPEND_TO_FILTER, COPY_TO_CLIPBOARD])
 
+    #: Provider of a scrollable view for the column name buttons
+    _scrollable_column_names = Instance(ScrollableButtonListView)
+
     def __init__(self, source_df: DataFrame, **traits):
         super(FilterExpressionEditorView, self).__init__(**traits)
         if not any(sorted(source_df)):
@@ -79,28 +122,43 @@ class FilterExpressionEditorView(HasStrictTraits):
                 raise KeyError(msg)
             self.included_cols = traits["included_cols"]
         self._create_trait_translators()
-        self._create_button_traits_from_column_names()
-        self._create_button_traits_from_column_values()
         self._edit_initialized = True
 
     def traits_view(self):
-        # create Items for each column name Button trait
+        # create a scrollable list of col name buttons
         col_names = sorted(self.source_df)
-        col_traited_names = list(itemgetter(*col_names)(self.traited_names))
-        col_name_bttns = [Item(n, show_label=False) for n in col_traited_names]
-        # create Items for each column value Button trait
-        included_column_button_list = self._create_included_col_view_tabs()
+        # create a {trait_name:name} dict for the ScrollableButtonListView
+        col_dict = {self.traited_names[name]: name for name in col_names}
+        self._scrollable_column_names = ScrollableButtonListView(
+            traits_and_names=col_dict, handler=self.filter_button_clicked
+        )
+
+        included_scroll_list = []
+        # create scrollable lists for column value buttons
+        for column in self.included_col_values.keys():
+            uniques = self.included_col_values[column]
+            # create a {trait_name:name} dict for the ScrollableButtonListView
+            col_dict = {self.traited_names[name]: name for name in uniques}
+            scroll_view = ScrollableButtonListView(
+                traits_and_names=col_dict, handler=self.filter_button_clicked
+            )
+            # make a trait for this ScrollableButtonListView
+            scroll_trait = f"{self.traited_names[column]}_col_scroll_list"
+            self.add_trait(scroll_trait, scroll_view)
+            # make an Item for the new scroll_trait
+            new_item = Item(scroll_trait, style="custom", show_label=False,
+                            editor=InstanceEditor(), label=column)
+            included_scroll_list.append(new_item)
 
         # make the view
         view = self.view_klass(
             HSplit(
                 VGroup(
                     Tabbed(
-                        VGroup(
-                            *col_name_bttns,
-                            label="Column Names"
-                        ),
-                        *included_column_button_list,
+                        Item("_scrollable_column_names", style="custom",
+                             editor=InstanceEditor(), show_label=False,
+                             label="Column Names"),
+                        *included_scroll_list
                     ),
                     HGroup(
                         Item('click_type', style='custom', show_label=False),
@@ -111,13 +169,13 @@ class FilterExpressionEditorView(HasStrictTraits):
                 ),
                 VGroup(
                     Item("expr", style="custom", show_label=False, width=450),
-                    show_border=True,
-                ),
+                    show_border=True
+                )
             ),
             resizable=True,
             buttons=OKCancelButtons,
             title="Edit the filter...",
-            statusbar=StatusItem(name="status_bar_message")
+            statusbar=StatusItem(name="status_bar_message"),
         )
         return view
 
@@ -142,46 +200,6 @@ class FilterExpressionEditorView(HasStrictTraits):
             self.traited_names[name] = trait_name
             # track the original name with a traited name key
             self.original_names[trait_name] = name
-
-    def _create_included_col_view_tabs(self):
-        """ Create a list of View Items for each value of each included column
-        """
-        tab_list = []
-        for column in self.included_col_values.keys():
-            uniques = self.included_col_values[column]
-            value_bttns = [Item(self.traited_names[name], show_label=False)
-                           for name in uniques]
-            new_group = VGroup(
-                *value_bttns,
-                label=column
-            )
-            tab_list.append(new_group)
-        return tab_list
-
-    def _create_button_traits_from_column_values(self):
-        """ Create `Button` traits for each value of each included_cols item
-        """
-        for cat_item in self.included_cols:
-            column_values = [str(x) for x in self.source_df[cat_item].unique()]
-            self._create_button_traits(column_values)
-
-    def _create_button_traits_from_column_names(self):
-        """ Create `Button` traits for every column name in the source df
-        """
-        self._create_button_traits(sorted(self.source_df))
-
-    def _create_button_traits(self, names):
-        """ Create `Button` traits for each item in an `Iterable`
-
-        Parameters
-        ----------
-        names : Iterable
-            Iterable of values from which to create a set of `Button` traits
-        """
-        for name in names:
-            trait_name = self.traited_names[name]
-            self.add_trait(trait_name, Button(name))
-            self.observe(self.filter_button_clicked, trait_name)
 
     def filter_button_clicked(self, event):
         """ Appends the clicked value to the filter or copies to the clipboard
@@ -216,14 +234,35 @@ if __name__ == "__main__":
     test_df = DataFrame(
         {
             "a": [1, 2, 4, 5],
-            "b_ff": list("zxyh"),
-            "c": list("test"),
-            "d": ["1 alpha", "MM_TT_BB_03-92", "2zeta", "kAPPa pappa"],
+            "b": list("zxyh"),
+            "c": list("zxyh"),
+            "d": list("zxyh"),
+            "e": list("zxyh"),
+            "f": list("zxyh"),
+            "g": list("zxyh"),
+            "h": list("zxyh"),
+            "i": list("zxyh"),
+            "j": list("zxyh"),
+            "k": list("zxyh"),
+            "l": list("zxyh"),
+            "m": list("zxyh"),
+            "n": list("zxyh"),
+            "o": list("zxyh"),
+            "p": list("zxyh"),
+            "q": list("zxyh"),
+            "r": list("zxyh"),
+            "s": list("zxyh"),
+            "t": list("zxyh"),
+            "u": list("zxyh"),
+            "v": list("zxyh"),
+            "w": list("zxyh"),
+            "x": list("test"),
+            "y": ["1 alpha", "MM_TT_BB_03-92", "2zeta", "kAPPa pappa"],
             "e_column": ["03_06 04_STANDARD.name",
                          "about this", "._ a93", "d!@#.$% ^&*()_+d"]
         },
         index=[0, 1, 3, 4]
     )
     test = FilterExpressionEditorView(expr="a == 4", source_df=test_df,
-                                      included_cols=["c", "e_column"])
+                                      included_cols=["y", "e_column", "w"])
     test.configure_traits()
