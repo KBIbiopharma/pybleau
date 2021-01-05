bc2253e5745e1b1bc5910e7c1bab36705f55ed56
Jonathan Rocher <jrocher@kbibiopharma.com>
2021-01-05 09:54:32 -0600

    Add unit tests for the new class.

---
+++ pybleau/app/plotting/tests/test_multi_mapper_plot.py
@@ -0,0 +1,20 @@
+from chaco.api import OverlayPlotContainer
+from  unittest import TestCase
+
+from pybleau.app.plotting.multi_mapper_plot import MultiMapperPlot
+from pybleau.app.plotting.plot_container_style import PlotContainerStyle
+
+
+class TestMultiMapperPlot(TestCase):
+    def test_create(self):
+        plot = MultiMapperPlot()
+        self.assertIsInstance(plot, OverlayPlotContainer)
+
+    def test_create_control_padding(self):
+        plot = MultiMapperPlot(left_padding=10)
+        self.assertIsInstance(plot, OverlayPlotContainer)
+
+    def test_create_control_attr_via_container_style(self):
+        attrs = PlotContainerStyle().to_traits()
+        plot = MultiMapperPlot(**attrs)
+        self.assertIsInstance(plot, OverlayPlotContainer)
