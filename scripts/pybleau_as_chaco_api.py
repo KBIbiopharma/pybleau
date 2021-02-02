39c583919fe3a4cc8bbb83f0578ea3d3e6f99fe9
Jonathan Rocher <jrocher@kbibiopharma.com>
2021-02-02 14:31:14 -0600

    Trivial.

--- scripts/pybleau_as_chaco_api.py
+++ scripts/pybleau_as_chaco_api.py
@@ -8,11 +8,11 @@ from pybleau.app.plotting.api import plot_from_config
 
 from pybleau.app.plotting.plot_config import HeatmapPlotConfigurator
 
-TEST_DF2 = DataFrame({"col 1": ["a", "b", "c", "a", "b", "c", "a", "b", "c"],
-                      "col 2": ["a", "a", "a", "b", "b", "b", "c", "c", "c"],
-                      "col 3":  [1,   2,   3,   2,   3,   4,   2,   1,   3]})
+TEST_DF = DataFrame({"col 1": ["a", "b", "c", "a", "b", "c", "a", "b", "c"],
+                     "col 2": ["a", "a", "a", "b", "b", "b", "c", "c", "c"],
+                     "col 3":  [1,   2,   3,   2,   3,   4,   2,   1,   3]})
 
-config1 = HeatmapPlotConfigurator(data_source=TEST_DF2, plot_title="plot 1",
+config1 = HeatmapPlotConfigurator(data_source=TEST_DF, plot_title="plot 1",
                                   x_col_name="col 1", y_col_name="col 2",
                                   z_col_name="col 3")
 
